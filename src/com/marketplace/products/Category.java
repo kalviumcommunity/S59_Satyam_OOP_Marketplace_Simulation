0ddd8c5a370b559bff22d6cd616910efddda09c6
Satyam <satyamworks20@gmail.com>
2024-09-20 15:50:22 +0530

    Abstraction Added

--- src/com/marketplace/products/Category.java
+++ src/com/marketplace/products/Category.java
@@ -1,43 +1,37 @@
 package com.marketplace.products;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Category {
     private String name;
     private ArrayList<Product> products;
 
-    public String getName(){
-       return name;
-    }
-
-    public Category(String name){
+    public Category(String name) {
         this.name = name;
         this.products = new ArrayList<>();
     }
 
-    public void addProduct(Product product){
+    public String getName() {
+        return name;
+    }
+
+    public void addProduct(Product product) {
         products.add(product);
     }
 
-    public void displayProducts(){
-        for(Product product : products){
+    public void displayProducts() {
+        for (Product product : products) {
             product.displayDetails();
         }
     }
 
-
-    public Product findProductBySerialNo(){
-        System.out.println("Find product by serial no");
-        Scanner scanner = new Scanner(System.in);
-        String serialNo = scanner.nextLine();
-        for(Product product : products){
-            if(product.getSerialNo().equals(serialNo)){
+    // Public method to find a product by serial number doing work of abstraction
+    public Product findProductBySerialNo(String serialNo) {
+        for (Product product : products) {
+            if (product.getSerialNo().equals(serialNo)) {
                 return product;
             }
-
         }
         return null;
     }
-
 }
